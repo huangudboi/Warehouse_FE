dc4e4ae3212e7bb526c1c70b7c389384a24404e0
huangudboi <huan.pv09082001@gmail.com>
2023-09-13 14:31:28 +0700

    dfdf login feature

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutViewVue from '../views/AboutView.vue'
+import LoginForm from '../views/signIn_singUp/LoginForm.vue'
+import RegisterVue from '../views/signIn_singUp/RegisterVue.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,10 +15,17 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutViewVue
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginForm
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterVue
     }
   ]
 })
